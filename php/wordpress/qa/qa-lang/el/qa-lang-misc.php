9aca7601d20f9d095cb663b6556b970fa0a848e6
SEzovalis <ezovalis@gmail.com>
2012-09-11 09:21:35 +0300

    question2answer Greek translation
    Version: 1.5.2

---
+++ php/wordpress/qa/qa-lang/el/qa-lang-misc.php
@@ -0,0 +1,96 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-include/el/qa-lang-misc.php
+	Version: 1.5.2
+	Date: 2012-30-08 13:42:51 GMT+2
+	Description: Miscellaneous language phrases in Greek
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'block_ip_button' => 'Αποκλεισμός IP διεύθυνσης',
+		'browse_categories' => 'Περιηγηθείτε στις κατηγορίες',
+		'captcha_confirm_fix' => 'Για να αποφύγετε αυτή την επιβεβαίωση στο μέλλοn, παρακαλώ ^5επιβεβαιώστε το email^6.',
+		'captcha_error' => 'Παρακαλώ συμπληρώστε την επιβεβαίωση anti-spam',
+		'captcha_label' => 'Επιβεβαίωση anti-spam:',
+		'captcha_login_fix' => 'Για να αποφύγετε αυτή την επιβεβαίωση στο μέλλοn, παρακαλώ κάνετε ^1εισαγωγή^2 ή ^3εγγραφή^4.',
+		'feed_a_edited_prefix' => 'Επεξεργασμένη απάντηση: ',
+		'feed_a_prefix' => 'Απαντημένη: ',
+		'feed_a_reshown_prefix' => 'Εκ νέου εμφάνιση απάντησης: ',
+		'feed_a_selected_prefix' => 'Επιλεγμένη απάντηση: ',
+		'feed_c_edited_prefix' => 'Επεξεργασμένο σχόλιο: ',
+		'feed_c_moved_prefix' => 'Μετακινημένο σχόλιο: ',
+		'feed_c_prefix' => 'Σχολιασμένη: ',
+		'feed_c_reshown_prefix' => 'Εκ νέου εμφάνιση σχολίου: ',
+		'feed_closed_prefix' => 'Κλεισμένη: ',
+		'feed_edited_prefix' => 'Επεξεργασμένη: ',
+		'feed_hidden_prefix' => 'Κρυμμένη: ',
+		'feed_not_found' => 'Δεν βρέθηκε Feed',
+		'feed_recategorized_prefix' => 'Επανα-κατηγοριοποιημένη: ',
+		'feed_reopened_prefix' => 'Επανα-ανοιγμένη: ',
+		'feed_reshown_prefix' => 'Εκ νέου εμφάνιση: ',
+		'feed_retagged_prefix' => 'Επανα-σημασμένη: ',
+		'feedback_email' => 'Email: (προαιρετικό)',
+		'feedback_empty' => 'Παρακαλώ χρησιμοποιήστε αυτό το πεδίο για να στείλετε σχόλια και προτάσεις',
+		'feedback_message' => 'Τα σχόλια και οι προτάσεις σας για το ^:',
+		'feedback_name' => 'Το όνομά σου: (προαιρετικό)',
+		'feedback_sent' => 'Το μήνυμά σας εστάλη - Ευχαριστούμε.',
+		'feedback_title' => 'Επικοινωνία - σχόλια',
+		'hide_all_ip_button' => 'Απόκρυψη των σχολίων από την παρακάτω IP',
+		'host_name' => 'Όνομα Host:',
+		'matches_blocked_ips' => 'Ταίριασμα με μπλοκαρισμένες διευθύνσεις IP:',
+		'message_empty' => 'Παρακαλώ εισάγετε το μήνυμα που θέλετε να στείλετε στον χρήστη',
+		'message_explanation' => 'Αυτό θα αποσταλεί ως κοινοποίηση από ^. Το email σας θα αποκαλυφθεί εκτός και εαν περιληφθεί στο μήνυμα.',
+		'message_for_x' => 'Το μήνυμά σας για ^:',
+		'message_limit' => 'Έχουν αποσταλεί πάρα πολλά προσωπικά μηνύματα - παρακαλω προσπαθήστε σε μια ώρα',
+		'message_must_login' => 'Παρακαλώ ^1συνδεθείτε^2 ή ^γραφτείτε^4 για να στέλνετε προσωπικά μηνύματα.',
+		'message_received_x_ago' => 'Ελλήφθει ^ πριν:',
+		'message_recent_history' => 'Πρόσφατη αλληλογραφία με ^',
+		'message_sent_x_ago' => 'Εστάλλει ^ πριν:',
+		'message_sent' => 'Το προσωπικό σας μήνυμα εστάλλει',
+		'my_favorites_title' => 'Τα αγαπημένα μου',
+		'nav_all_my_updates' => 'Όλες μου οι ενημερώσεις',
+		'nav_my_content' => 'Το περιεχόμενο μου',
+		'nav_my_details' => 'Οι λεπτομέρειες μου',
+		'nav_my_favorites' => 'Τα αγαπημένα μου',
+		'no_activity_from_x' => 'Χωρίς δραστηριότητα από την ^',
+		'no_favorite_categories' => 'Χωρίς αγαπημένες κατηγορίες',
+		'no_favorite_qs' => 'Χωρίς αγαπημένες ερωτήσεις',
+		'no_favorite_tags' => 'Χωρίς αγαπημένες ετικέτες',
+		'no_favorite_users' => 'Χωρίς αγαπημένους χρήστες',
+		'no_recent_updates' => 'Χωρίς πρόσφατες ενημερώσεις',
+		'no_updates_content' => 'Χωρίς πρόσφατες ενημερώσεις για το περιεχόμενό μου',
+		'no_updates_favorites' => 'Χωρίς ενημερώσεις για τα αγαπημένα μου',
+		'private_message_title' => 'Αποστολή προσωπικού μηνύματος',
+		'recent_activity_from_x' => 'Πρόσφατη δραστηριότηυτα από την ^',
+		'recent_updates_content' => 'Πρόσφατες ενημερώσεις για το περιεχόμενό μου',
+		'recent_updates_favorites' => 'Πρόσφατες ενημερώσεις για τα αγαπημένα μου',
+		'recent_updates_title' => 'Πρόσφατες ενημερώσεις για εμένα',
+		'site_in_maintenance' => 'Το website είναι προσωρινά μη διαθέσιμο λόγω συντήρησης - δοκιμάστε ξανά σύντομα.',
+		'suggest_favorites_add' => 'Για την προσθήκη ερώτησης ή άλλου αντικειμένου στα αγαπημένα μου, πατήστε το ^ στην κορυφή της σελίδας.',
+		'suggest_update_favorites' => 'Για περισσότερες ενημερώσεις, προσθέστε αντικείμενα στα ^1αγαπημένα μου^2.',
+		'unblock_ip_button' => 'Επαναφορά IP διεύθυνσης',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
---
+++ php/wordpress/qa/qa-lang/el/qa-lang-question.php
@@ -0,0 +1,82 @@
+<?php	/*	Question2Answer (c) Stamatis Ezovalis	http://www.question2answer.org/		File: qa-include/el/qa-lang-question.php	Version: 1.5.2	Date: 2012-30-08 13:42:51 GMT+2	Description: Greek language phrases for question page	This program is free software; you can redistribute it and/or	modify it under the terms of the GNU General Public License	as published by the Free Software Foundation; either version 2	of the License, or (at your option) any later version.		This program is distributed in the hope that it will be useful,	but WITHOUT ANY WARRANTY; without even the implied warranty of	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	GNU General Public License for more details.	More about this license: http://www.question2answer.org/license.php*/	return array(		'1_answer_title' => '1 Απάντηση',
+		'a_convert_to_c_on' => 'Μετέτρεψε αυτή την απάντηση σε σχόλιο σε:',
+		'a_convert_to_c' => 'Μετέτρεψε αυτή την απάντηση σε σχόλιο',
+		'a_convert_warn_cs' => 'Προσοχή: Αυτή η μετατροπή είναι μή αναστρέψιμη και θα μετακινήσει και τα σχόλια των απαντήσεων.',
+		'a_convert_warn' => 'Προσοχή: Αυτή η μετατροπή είναι μη αναστρέψιμη.',
+		'a_notify_email' => 'Στείλε μου email σε αυτή τη διεύθυνση αν επιλεχθεί ή σχολιασθεί η απάντησή μου:',
+		'a_notify_label' => 'Στείλε μου email αν επιλεχθεί ή σχολιασθεί η απάντησή μου',
+		'a_notify_x_label' => 'Στείλε μου email (^) αν επιλεχθεί ή σχολιασθεί η απάντησή μου',
+		'a_waiting_your_approval' => 'Αυτή η απάντηση περιμένει την έγκριση σας',		'a_your_waiting_approval' => 'Η απάντηση σας θα ελεγχθεί και θα εγκριθεί σύντομα.',		'add_answer_button' => 'Προσθήκη απάντησης',
+		'add_comment_button' => 'Προσθήκη σχολίου',
+		'add_q_favorites' => 'Προσθήκη της ερώτησης στα αγαπημένα μου',		'answer_button' => 'απάντηση',
+		'answer_limit' => 'Δόθηκαν πάρα πολλές απαντήσεις - παρακαλώ δοκιμάστε σε μια ώρα',
+		'answer_must_confirm' => 'Παρακαλώ ^5επιβεβαιώστε το email^6 για να απαντήσετε σε αυτή την ερώτηση.',
+		'answer_must_login' => 'Παρακαλώ ^1συνδεθείτε^2 ή ^3εγγραφείτε^4 για να απαντήσετε στην ερώτηση.',
+		'answer_q_popup' => 'Απάντησε αυτή την ερώτηση',
+		'approve_button' => 'έγκριση',		'ask_button' => 'Κάνε την ερώτηση',
+		'ask_follow_from_a' => 'Η ερώτησή σας θα συσχετισθεί με αυτή την απάντηση:',
+		'ask_follow_title' => 'Κάνε μια σχετική ερώτηση',
+		'ask_limit' => 'Έγιναν πάρα πολλές ερωτήσεις - παρακαλώ δοκιμάστε σε μια ώραplease try again in an hour',
+		'ask_must_confirm' => 'Παρακαλώ ^5επιβεβαιώστε το email σας^6 για να κάνετε μια ερώτηση.',
+		'ask_must_login' => 'Παρακαλώ ^1συνδεθείτε^2 ή ^3εγγραφείτε^4 για να κάνετε μια ερώτηση.',
+		'ask_same_q' => 'Πρινι προχωρήσετε, παρακαλώ ελέγξτε αν η ερώτηση έχει γίνει ήδη:',
+		'ask_title' => 'Κάνε μια ερώτηση',
+		'c_notify_email' => 'Στείλε μου email σε αυτή τη διεύθυνση αν προστεθεί κάποιο σχόλιο μετά το δικό μου:',
+		'c_notify_label' => 'Στείλε μου email αν προστεθεί κάποιο σχόλιο μετά το δικό μου',
+		'c_notify_x_label' => 'Στείλε μου email (^) αν προστεθεί κάποιο σχόλιο μετά το δικό μου',
+		'c_waiting_your_approval' => 'Αυτό το σχόλιο περιμένει την έγκριση σας',		'c_your_waiting_approval' => 'Το σχόλιο σας θα ελεγχθεί και θα εγκριθεί σύντομα.',		'category_js_note' => 'Για να επιλέξετε μια κατηγορία, ενεργοποιήστε τη Javascript στον περιηγητή σας.',		'category_required' => 'Παρακαλώ επιλέξτε κατηγορία',		'claim_button' => 'εγώ το έγραψα',
+		'clear_flags_button' => 'καθαρισμός flags',		'clear_flags_popup' => 'Αφαιρέστε τα flags από όλους τους χρήστες',		'close_button' => 'κλείσε',		'close_duplicate_error' => 'Η διπλότυπη ερώτηση δε μπόρεσε να βρεθεί - παρακαλώ εισάγετε τον αριθμό URL από μια διαφορετική ερώτηση, π.χ. 123.',		'close_duplicate' => 'Αυτή είναι διπλότυπη άλλης ερώτησης',		'close_form_button' => 'Κλείσιμο ερώτησης',		'close_form_title' => 'Κλείσε αυτή την ερώτηση',		'close_original_note' => 'Μπορείτε επίσης να εισάγετε τον αριθμό ερώτησης από το URL, π.χ. 123.',		'close_original_title' => 'URL της πρωτότυπης ερώτησης:',		'close_q_popup' => 'Κλείσιμο της ερώτησης σε κάθε νέα ερώτηση απάντηση',		'close_reason_title' => 'Λόγος κλεισίματος της ερώτησης:',		'closed_as_duplicate' => 'έκλεισε ως διπλότυπη της:',		'closed_with_note' => 'έκλεισε με την υποσημείωση:',		'comment_a_popup' => 'Προσθήκη σχολίου σε αυτή την απάντηση',
+		'comment_button' => 'σχόλιο',
+		'comment_limit' => 'Πάρα πολλά σχόλια - παρακαλώ δοκιμάστε ξανά σε μια ώρα',
+		'comment_must_confirm' => 'Παρακαλώ ^5επιβεβαιώστε το email σας^6 για να προσθέσετε σχόλιο.',
+		'comment_must_login' => 'Παρακαλώ ^1συνδεθείτε^2 ή ^3εγγραφείτε^4 για να προσθέσετε σχόλιο.',
+		'comment_on_a' => 'Στην απάντηση: ',
+		'comment_on_q' => 'Στην ερώτηση: ',
+		'comment_q_popup' => 'Προσθήκη σχολίου σε αυτή την ερώτηση',
+		'delete_a_popup' => 'Μόνιμη διαγργαφή αυτής της απάντησης',
+		'delete_button' => 'διαγραφή',
+		'delete_c_popup' => 'Μόνιμη διαγραφή αυτού του σχολίου',
+		'delete_q_popup' => 'Μόνιμη διαγραφή αυτής της ερώτησης',
+		'duplicate_content' => 'Η εγγραφή σας φαίνεται να είναι διπλή.',
+		'edit_a_popup' => 'Επεξεργασία αυτής της απάντησης',
+		'edit_a_title' => 'Επεξεργασία απάντησης',
+		'edit_button' => 'επεξεργασία',
+		'edit_c_popup' => 'Επεξεργασία αυτού του σχολίου',
+		'edit_c_title' => 'Επεξεργασία σχολίου',
+		'edit_must_confirm' => 'Παρακαλώ ^5επιβεβαιώστε το email σας^6 για να το επεξεργαστείτε.',
+		'edit_must_login' => 'Παρακαλώ ^1συνδεθείτε^2 ή ^3κάντε εγγραφή^4 για να το επεξεργαστείτε.',
+		'edit_q_popup' => 'Επεξεργασία αυτής της ερώτησης',
+		'edit_q_title' => 'Επεξεργασία ερώτησης',
+		'example_tags' => 'Προτεινόμενα tags: ',
+		'flag_a_popup' => 'Σήμανε αυτή την απάντηση ως spam ή ανάρμοστη',		'flag_button' => 'σήμανε',		'flag_c_popup' => 'Σήμανε αυτό το σχόλιο ως spam ή ανάρμοστο',		'flag_hide_button' => 'σήμανε και κρύψε',		'flag_limit' => 'Πολλές σημασμένες δημοσιευσεις - παρακαλώ προσπαθήστε ξανά σε μια ώρα',		'flag_must_confirm' => 'Παρακαλώ ^5επιβεβαιώστε το email^6 για να σημαίνεται δημοσιεύσεις.',		'flag_must_login' => 'Παρακαλώ ^1συνδεθείτε^2 ή ^3γραφτείτε^4 για να σημαίνεται δημοσιεύσεις.',		'flag_not_allowed' => 'Η σήμανση αυτή δεν επιτρέπετε',		'flag_q_popup' => 'Σήμανε αυτή την ερώτηση ως spam ή ανάρμοστη',		'follow_a_popup' => 'Κάνε μια νέα ερώτηση σχετική με την απάντηση',
+		'follow_button' => 'κάνε σχετική ερώτηση',
+		'follows_a' => 'συσχετισμένη με την απάντηση για:',
+		'follows_q' => 'σχετικά με την ερώτηση:',
+		'hide_a_popup' => 'Απόκρυψη αυτής της απάντησης',
+		'hide_button' => 'απόκρυψη',
+		'hide_c_popup' => 'Απόκρυψη αυτού του σχολίου',
+		'hide_q_popup' => 'Απόκρυψη αυτής της ερώτησης',
+		'matching_tags' => 'Tags που ταιριάζουν: ',
+		'max_tags_x' => 'Επιτρέπονται το πολύ ^ tags',
+		'min_tags_x' => 'Παρακαλώ δώστε τουλάχιστον ^ tag/s',
+		'notify_email_note' => 'Privacy: Η διεύθυνση του email σας θα χρησιμοποιηθεί μόνο για να σταλούν αυτές οι σημειώσεις.',
+		'q_category_label' => 'Κατηγορία:',
+		'q_content_label' => 'Περισσότερη πληροφορία για την ερώτησή σας:',
+		'q_hidden_author' => 'Αυτή η ερώτηση έχει αποκρυφθεί από τον χρήστη που την έθεσε',
+		'q_hidden_flagged' => 'Αυτή η ερώτηση έχει σημανθεί και κρυφτεί',		'q_hidden_other' => 'Αυτή η ερώτηση έχει αποκρυφθεί',
+		'q_notify_email' => 'Στείλε μου email σε αυτή τη διεύθυνση αν η ερώτησή μου απαντηθεί ή σχολιαστεί:',
+		'q_notify_label' => 'Στείλε μου email αν η ερώτησή μου απαντηθεί ή σχολιαστεί',
+		'q_notify_x_label' => 'Στείλε μου email (^) αν υπάρχει απάντηση ή σχόλιο στην ερώτησή μου',
+		'q_tags_comma_label' => 'Ετικέτες - χρησιμοποίησε το κόμμα (,) ως διαχωριστικό:',		'q_tags_label' => 'Ετικέτες - χρησιμοποιείστε παύλες για να συνδυάσετε λέξεις:',
+		'q_title_label' => 'Η ερώτησή σας σε μια πρόταση:',
+		'q_waiting_approval' => 'Αυτή η ερώτηση περιμένει για έγκριση',		'q_waiting_your_approval' => 'Αυτή η ερώτηση περιμένει για τη δική σας έγκριση',		'q_your_waiting_approval' => 'Η ερώτηση σας θα ελεγχθεί και θα εγκριθεί σύντομα.',		'recat_button' => 'επανα-κατηγοριοποίησε',		'recat_popup' => 'Άλλαξε την κατηγορία της/των ερώτησης/σεων',		'recat_q_title' => 'Επανα-κατηγοριοποίηση ερώτησης',		'reject_button' => 'απόρριψη',		'remove_q_favorites' => 'Αφαίρεσε την ερώτηση από τα αγαπημένα μου',		'reopen_button' => 'Άνοιξε ξανά',		'reply_button' => 'απάντηση',
+		'reply_c_popup' => 'Απάντηση σε αυτό το σχόλιο',
+		'reshow_button' => 'επανεμφάνιση',
+		'retag_button' => 'retag',		'retag_cat_popup' => 'Άλλαξε την κατηγορία ή τις ετικέτες της/των ερώτησης/σεων',		'retag_popup' => 'Άλλαξε τις ετικέτες της/των ερώτησης/σεων',		'retag_q_title' => 'Retag ερώτησης',		'select_popup' => 'Κάντε κλικ για να την επιλέξετε σαν καλύτερη απάντηση',
+		'select_text' => 'Καλύτερη απάντηση',
+		'show_1_comment' => 'Προβολή 1 σχόλιου',		'show_1_previous_comment' => 'Προβολή 1 προηγούμενου σχολίου',		'show_x_comments' => 'Προβολή ^ σχολίων',		'show_x_previous_comments' => 'Προβολή ^ προηγούμενων σχολίων',		'unflag_button' => 'unflag',		'unflag_popup' => 'Αφαίρεσε τη σήμανση που πρόσθεσες',		'unselect_popup' => 'Κάντε κλικ για να αφαιρέσετε την επιλογή',
+		'x_answers_title' => '^ Απαντήσεις',
+		'your_answer_title' => 'Η απάντησή σας',
+		'your_comment_a' => 'Το σχόλιό σας σε αυτή την απάντηση:',
+		'your_comment_q' => 'Το σχόλιό σας σε αυτή την ερώτηση:',
+	);	/*	Omit PHP closing tag to help avoid accidental output*/
