9b5c71da9553caeb9d250078946a36c1a090b09e
Makis Antypas <hamlet.is.away@gmail.com>
2012-09-22 01:45:15 +0300

    cosmetic changes to new incident page

--- php/wordpress/wp-content/themes/pavlatheme/category-16.php
+++ php/wordpress/wp-content/themes/pavlatheme/category-16.php
@@ -21,20 +21,21 @@ get_header();
     }
 </script>
 
-
-
     <div class="content">
         <h1 class="exeisimvikaisemena-icon"><?php echo single_cat_title( $category_id ); ?></h1>
         <div class="subtext"><?php echo category_description( $category_id );  ?></div>
         <div class="text exeisimvikaisemena">
+	<a href="?cat=18">
            <div class="left diafthora" ><img src="images/exeisimvikaisemena/diafthora.png" onclick="SelectType('bad')" onmouseover="SelectType('bad')"  />
                <img src="images/exeisimvikaisemena/tick.png" class="tick" data-type="bad" />
            </div>
+	</a>
            <div class="left or" ><img src="images/exeisimvikaisemena/or.png" /></div>
+	<a href="?cat=19">
            <div class="left peristatiko"><img src="images/exeisimvikaisemena/peristatiko.png" onclick="SelectType('good')" onmouseover="SelectType('good')"  />
            <img src="images/exeisimvikaisemena/tick.png" class="tick" data-type="good" />
            </div>
-            
+      </a>
            <div class="clear"></div>
            <div  class="left next-left"><a href="?cat=18"><img border="0" src="images/btns/next.png" data-type="bad" /></a></div>
            <div class="left next-right"><a href="?cat=19"><img border="0" src="images/btns/next.png" data-type="good" /></a></div>
