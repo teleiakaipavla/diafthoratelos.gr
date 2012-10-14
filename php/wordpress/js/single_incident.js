f10a64a3eebf35059dad7922b514d4319d4f8a8b
Panos Louridas <louridas@gmail.com>
2012-10-14 18:01:03 +0300

    fixes #62

--- php/wordpress/js/single_incident.js
+++ php/wordpress/js/single_incident.js
@@ -3,77 +3,81 @@ $(document).ready(function () {
 	//var DataUrl = 'datasource/incident.htm?rnd=' + Math.random(100000);
 	
     $.getJSON(DataUrl, function (data) {
-                        var place = data.place == undefined ? "" : data.place.name;
-			var public_entity = data.public_entity.name
-			var longitude = data.place.longitude
-			var latitude = data.place.latitude 
-			var desc = data.description
-			var category = data.public_entity.category.name
-			var date = data.incident_date
-			var money_asked = groupThousands(Math.round( data.money_asked ))
-			var money_given = groupThousands(Math.round( data.money_given )) 
-			
-			var praise = data.praise
-			if (data.praise == 1){
-					$('#incident_type').append('Καλό νέο')
-					$('#more_link').append('<a href="?cat=14">Δες περισσότερα καλά νέα</α>')
-					$('#money_details').empty()
-					$("#incid_desc").attr("class", "PeristatikoIn-TextLeft-big");
-					$("#incident_type").attr("class", "goodnews-icon");
-					document.title = 'Καλό νέο για ' + public_entity;
-					
-				}
-				else
-				{
-					$('#incident_type').append('Περιστατικό διαφθοράς')	
-					$('#more_link').append('<a href="?cat=20">Δες περισσότερα περιστατικά διαφθοράς</α>')
-					$("#incident_type").attr("class", "protoimediafora-icon");
-					document.title = 'Περιστατικό διαφθοράς σε ' + public_entity;
-					
-				};
-			
-			var incid_meta = category + ' | ' + place + ' | ' + public_entity + ' | <b>' + date + '</b>'		
-			var meta_div = document.createElement("div")
-	        $(meta_div).append(incid_meta)
-            $('#incid_meta').append(meta_div)
+        var placeName = "";
+        var longitude = 23.824312;
+        var latitude = 38.074208;
+        var placeDefined = data.place != undefined;
+        if (placeDefined) {
+            placeName = data.place.name;
+            longitude = data.place.longitude
+            latitude = data.place.latitude 
+        }
+        var publicEntity = data.public_entity.name
+        var desc = data.description
+        var category = data.public_entity.category.name
+        var date = data.incident_date
+        var moneyAsked = groupThousands(Math.round( data.money_asked ))
+        var moneyGiven = groupThousands(Math.round( data.money_given )) 
+                        
+        var praise = data.praise
+        if (data.praise == 1){
+            $('#incident_type').append('Καλό νέο')
+            $('#more_link').append('<a href="?cat=14">Δες περισσότερα καλά νέα</α>')
+            $('#money_details').empty()
+            $("#incid_desc").attr("class", "PeristatikoIn-TextLeft-big");
+            $("#incident_type").attr("class", "goodnews-icon");
+            document.title = 'Καλό νέο για ' + publicEntity;
             
-			var desc_div = document.createElement("div")
-			$(desc_div).append(desc)
-            $('#incid_desc').append(desc_div)
-			
-			var incid_asked_div = document.createElement("div")
-			$(incid_asked_div).append(money_asked)
-		    $('#incid_asked').append(incid_asked_div)
-			
-			var incid_given_div = document.createElement("div")
-			$(incid_given_div).append(money_given)
-		    $('#incid_given').append(incid_given_div)
-			
-			showOnMap(latitude, longitude, place)
+        } else {
+            $('#incident_type').append('Περιστατικό διαφθοράς') 
+            $('#more_link').append('<a href="?cat=20">Δες περισσότερα περιστατικά διαφθοράς</α>')
+            $("#incident_type").attr("class", "protoimediafora-icon");
+            document.title = 'Περιστατικό διαφθοράς σε ' + publicEntity;
+            
+        };
+        
+        var incidMeta = category + ' | ' + placeName + ' | ' + publicEntity + ' | <b>' + date + '</b>'            
+        var metaDiv = document.createElement("div")
+        $(metaDiv).append(incidMeta)
+        $('#incid_meta').append(metaDiv)
+        
+        var desc_div = document.createElement("div")
+        $(desc_div).append(desc)
+        $('#incid_desc').append(desc_div)
+                        
+        var incidAskedDiv = document.createElement("div")
+        $(incidAskedDiv).append(moneyAsked)
+        $('#incid_asked').append(incidAskedDiv)
+        
+        var incidGivenDiv = document.createElement("div")
+        $(incidGivenDiv).append(moneyGiven)
+        $('#incid_given').append(incidGivenDiv)
 
+        showOnMap(latitude, longitude, placeName, placeDefined);
+    })
+});
 
-        })
-    });
-
-function showOnMap(latitude, longitude, placename){
-  var myLatlng = new google.maps.LatLng(latitude, longitude);
-  var mapOptions = {
-    zoom: 12,
-    center: myLatlng,
-	draggable:true,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-	
+function showOnMap(latitude, longitude, placeName, putMarker) {
+    var myLatLng = new google.maps.LatLng(latitude, longitude);
+    var mapOptions = {
+        zoom: putMarker? 12 : 5,
+        center: myLatLng,
+        draggable: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+        
   }
 
-  var map = new google.maps.Map(document.getElementById("googlemaps"), mapOptions);
+    var map = new google.maps.Map(document.getElementById("googlemaps"),
+                                  mapOptions);
 
-
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title:placename
-  });
-marker.setMap(map);
+    if (putMarker) {
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: placeName
+        });
+        marker.setMap(map);
+    }
 }
 
 
